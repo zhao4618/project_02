bb54bfa3a21729ea60489916703b43be0a103f7e
zhaozhuo <2424318315@qq.com>
2022-04-20 18:56:24 +0800

    第一次提交

---
+++ lx.js
@@ -0,0 +1,28 @@
+//  jq入口函数
+// $(function() {;
+//     (function() {
+//         $('button').click(function() {
+//             alert('加油')
+//         })
+//     })()
+// })
+// $(window).on('load', function() {
+//         (function() {
+//             $('button').click(function() {
+//                 alert('加油!!')
+//             })
+//         })()
+//     })
+//  js入口函数
+// window.addEventListener('load', function() {
+//     let btn = document.querySelector('button')
+//     btn.addEventListener('click', function() {
+//         alert('加油!!!')
+//     })
+// })
+// document.addEventListener('DOMContentLoaded', function() {
+//     let btn = document.querySelector('button')
+//     btn.addEventListener('click', function() {
+//         alert('加油!!!123')
+//     })
+// })
